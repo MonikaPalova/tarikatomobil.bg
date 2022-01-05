b5146e417c784db942d97d011904d873bc7d2873
Borislav-K <bobissss84@gmail.com>
2022-01-05 19:55:00 +0100

    Serve UI

--- cmd/main.go
+++ cmd/main.go
@@ -30,6 +30,11 @@ func main() {
 	router.Path("/photos/{id}").Methods(http.MethodGet).HandlerFunc(photosHandler.GetPhoto)
 	router.Path("/photos/{id}").Methods(http.MethodDelete).HandlerFunc(photosHandler.DeletePhoto)
 
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./ui")))
+	//router.Path("/styles").Handler(http.FileServer(http.Dir("./ui/styles")))
+	//router.Path("/scripts").Handler(http.FileServer(http.Dir("./ui/scripts")))
+	//router.Path("/images").Handler(http.FileServer(http.Dir("./ui/images")))
+
 	if err := http.ListenAndServe(":80", router); err != nil {
 		log.Fatalf("Could not start server: %s", err.Error())
 	}
